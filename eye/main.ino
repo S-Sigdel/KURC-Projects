169150cca198f941af9b7dd58f3b2ffec4303497
S-Sigdel <sakshyamsigdel@gmail.com>
2024-05-05 19:03:10 +0545

    added necessary header files

--- eye/main.ino
+++ eye/main.ino
@@ -1,28 +1,29 @@
-void calibrate() {}
+#include<Servo.h>
+Servo eyelidTopRight, eyelidTopLeft, yaw, pitch, eyelidBottomRight, eyelidBottomLeft;
+
 void setup() {
-
-  servo eyelidTopRight, eyelidTopLeft, yaw, pitch, eyelidBottomRight,
-        eyelidBottomLeft;
-
   eyelidTopRight.attach(2);
   eyelidTopLeft.attach(3);
   eyelidBottomRight.attach(4);
   eyelidBottomLeft.attach(5);
   yaw.attach(6);
   pitch.attach(7);
-}
-void looop(){
-  int firstcali=1;
-  if(firstcali){
 
+  //initial calibrate
+  calibrate();
+}
+
+void loop(){
+
+}
+
+void calibrate() {
     eyelidBottomLeft.write(60);
     eyelidBottomRight.write(60);
     eyelidTopLeft.write(0);
     eyelidTopRight.write(0);
     yaw.write(90);
     pitch.write(100);
-
   }
-  firstcali++;
 
-}
+
