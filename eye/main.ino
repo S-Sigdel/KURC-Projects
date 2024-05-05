877d6c2be14bcaf9534d98f3cdec60c57c5940e0
S-Sigdel <sakshyamsigdel@gmail.com>
2024-05-06 01:39:50 +0545

    feature for left-right, up-down

--- eye/main.ino
+++ eye/main.ino
@@ -1,9 +1,17 @@
 #include<Servo.h>
-//function declaration
+//definition for the value from the user
 #define numberOfServo 6
 #define digitsPerValue 3 //angle can be max of 3 digits
 
+//function declaration
 void calibrate();
+void close();
+void open();
+void lookLeft();
+void lookRight();
+void lookCenter();
+void lookUp();
+void lookDown();
 //global variable declaration to be used in the programs
 
 Servo eyelidTopRight, eyelidTopLeft, yaw, pitch, eyelidBottomRight, eyelidBottomLeft;
@@ -34,12 +42,42 @@ void loop(){
 }
 
 void calibrate() {
-    eyelidBottomLeft.write(60);
-    eyelidBottomRight.write(60);
-    eyelidTopLeft.write(0);
-    eyelidTopRight.write(0);
-    yaw.write(90);
-    pitch.write(100);
+  eyelidBottomLeft.write(60);
+  eyelidBottomRight.write(60);
+  eyelidTopLeft.write(0);
+  eyelidTopRight.write(0);
+  yaw.write(90);
+  pitch.write(100);
+}
+
+void close(){
+  eyelidBottomRight.startEaseTo(0);
+  eyelidBottomLeft.startEaseTo(0);
+  eyelidTopLeft.startEaseTo(60);
+  eyelidTopRight.startEaseTo(60);
+}
+void open(){
+  eyelidBottomRight.startEaseTo(60);
+  eyelidBottomLeft.startEaseTo(60);
+  eyelidTopLeft.startEaseTo(0);
+  eyelidTopRight.startEaseTo(0);
+
+}
+void lookLeft(){
+  yaw.startEaseTo(70);
+}
+void lookRight(){
+  yaw.startEaseTo(110);
+}
+void lookCenter(){
+  yaw.startEaseTo(90);
+}
+void lookUp(){
+  pitch.startEaseTo(120);
+}
+void lookDown(){
+  pitch.startEaseTo(80);
+}
+void lookCenter(){
+    pitch.startEaseTo(100);
   }
-
-
