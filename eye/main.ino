957bf8e6cb65a2fed8634c41f7131ad0781fe22e
S-Sigdel <sakshyamsigdel@gmail.com>
2024-05-05 17:52:47 +0545

    added inside loop function

--- eye/main.ino
+++ eye/main.ino
@@ -2,7 +2,7 @@ void calibrate() {}
 void setup() {
 
   servo eyelidTopRight, eyelidTopLeft, yaw, pitch, eyelidBottomRight,
-      eyelidBottomLeft;
+        eyelidBottomLeft;
 
   eyelidTopRight.attach(2);
   eyelidTopLeft.attach(3);
@@ -10,12 +10,19 @@ void setup() {
   eyelidBottomLeft.attach(5);
   yaw.attach(6);
   pitch.attach(7);
+}
+void looop(){
+  int firstcali=1;
+  if(firstcali){
 
-  // calibrate on first run:
-  eyelidBottomLeft.write(60);
-  eyelidBottomRight.write(60);
-  eyelidTopLeft.write(0);
-  eyelidTopRight.write(0);
-  yaw.write(90);
-  pitch.write(100);
+    eyelidBottomLeft.write(60);
+    eyelidBottomRight.write(60);
+    eyelidTopLeft.write(0);
+    eyelidTopRight.write(0);
+    yaw.write(90);
+    pitch.write(100);
+
+  }
+  firstcali++;
+
 }
